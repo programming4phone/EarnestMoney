fa56d78aade5153c52a944eb32ec8578db2eaae9
James Bell <jamesbell01@yahoo.com>
2018-01-25 10:30:31 -0500

    modified comments

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
     ganachedevelopment: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "5777" // Match any network id
+      network_id: "5777" 
     }
   }
 };
